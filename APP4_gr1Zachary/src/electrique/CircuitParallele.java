02d287be6c41a59e59247c8da359410fa99576ad
2542153 <2542153@cegeplimoilou.ca>
2026-04-07 21:27:12 -0400

    -effacage test pour calculerResistance() de CircuitParallele() -correction methode calculerResistance() de la classe CircuitParallele()

--- APP4_gr1Zachary/src/electrique/CircuitParallele.java
+++ APP4_gr1Zachary/src/electrique/CircuitParallele.java
@@ -7,29 +7,20 @@ import java.util.Scanner;
 
 public class CircuitParallele extends Circuit {
 
-    public static void main(String[] args) {
-        ArrayList<Composant> comp = new ArrayList<>();
-        comp.add(new Resistance(10000));
-        comp.add(new Resistance(2000));
-        comp.add(new Resistance(1000));
-
-        CircuitParallele cp = new CircuitParallele(comp);
-        double resultat = cp.calculerResistance();
-        System.out.println(resultat);
-    }
-
     public CircuitParallele(List<Composant> composants) {
         super(composants);
-
     }
 
     @Override
     public double calculerResistance() {
-        double resistanceTotal=0;
-        for (Composant c :getComposants() ){
-            resistanceTotal+=1/c.calculerResistance();
+        double resistanceTotal = 0;
+        for (Composant c : getComposants()) {
+            double resCheque = c.calculerResistance();
+            if (resCheque > 0) {
+                resistanceTotal += 1 / resCheque;
+            }
         }
-        return 1.0/resistanceTotal;
+        return 1 / resistanceTotal;
     }
-    }
+}
 
